a36b8d7ef8944bbe2218ce733bc303b7964f773d
Rohit Tamidapati <rohittamidapati11@gmail.com>
2023-05-20 18:34:57 +0530

    changes to color_text.js

--- color_text.js
+++ color_text.js
@@ -3,7 +3,6 @@ const color = require('colors');
 console.log(color.red('This is in red'));
 console.log(color.blue('This is in blue'));
 
-<<<<<<< HEAD
+
 console.log("This text is underlined".underline.green);
-=======
->>>>>>> 58094ab34149f4e92af4fe048f9ab2c166e6b840
+
